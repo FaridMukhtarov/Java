119f4c5fa9929afd05244c69a8c0da2d49555974
Farid Mukhtarov <farid@Farids-Mac-mini.local>
2023-04-27 09:47:27 +0400

    Java

---
+++ src/lesson02_DataTypeAndScanner/DataTypeNonPrimitives.java
@@ -0,0 +1,17 @@
+package lesson02_DataTypeAndScanner;
+
+public class DataTypeNonPrimitives {
+    public static void main(String[] args) {
+
+        // Non-Primitive Data Types
+        String name = "Farid Mukhtarov";
+        System.out.println(name.toUpperCase()); // FARID MUKHTAROV
+
+        // Class
+        // Object
+        // String
+        // Array
+        // Interface
+        // Enums
+    }
+}
---
+++ src/lesson02_DataTypeAndScanner/DataTypePrimitives.java
@@ -0,0 +1,36 @@
+package lesson02_DataTypeAndScanner;
+
+public class DataTypePrimitives {
+    public static void main(String[] args) {
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        char c1 = 's';
+        char c2 = 'H';
+        char c3 = '4';
+        char c4 = '*';
+        char c5 = ' ';
+
+        // Primitive data type containing integers
+        byte number1 = 3; // 8 bit memory
+        short number2 = 3; // 16 bit memory
+        int number = 5; // 32 bit memory
+        long number4 = 33; // 64 bit memory
+
+
+        // Primitive data type decimal
+        double d = 122.4;
+        float f = 3537.43f;
+
+
+
+
+
+
+
+
+
+
+    }
+}
