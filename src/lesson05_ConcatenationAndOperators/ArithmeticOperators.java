3acb75b3597208007786a71ef4beb8aee3d32603
Farid Mukhtarov <farid@Farids-Mac-mini.local>
2023-04-29 17:43:08 +0400

    Java

--- src/lesson05_ConcatenationAndOperators/ArithmeticOperators.java
+++ src/lesson05_ConcatenationAndOperators/ArithmeticOperators.java
@@ -3,5 +3,18 @@ package lesson05_ConcatenationAndOperators;
 public class ArithmeticOperators {
     public static void main(String[] args) {
 
+        //  Arithmetic Operators  +, -, *, /, %, ++, --
+
+        int a = 23;
+        int b = 33;
+
+        int netice = a + b;
+        System.out.println(netice);
+
+        int netice2 = a - b;
+        System.out.println(netice2);
+
+        int netice3 = (a*b) / (b+a);
+        System.out.println(netice3);
     }
 }
